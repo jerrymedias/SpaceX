8730d20fa04150fb258eaad6d19cbc8bee95b577
Ravi raj <ravi@ravi.raj>
2020-09-09 21:48:31 +0530

    changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SpaceX'));
+app.use(express.static(__dirname + '/dist/SpaceX/browser'));
 
 app.get('/*', function(req,res) {
     
